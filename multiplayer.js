132bda32f64b0a3926189a986c2fb5baf5e47340
Benjamin Park <parkbenjamin2@gmail.com>
2023-12-09 20:21:05 -0500

    git fix ready up

--- multiplayer.js
+++ multiplayer.js
@@ -182,7 +182,7 @@ function mpBattleGameLoop(timeMs) {
 							socket.send(JSON.stringify({
 								action: "READY_UP",
 								id: battleState.id,
-								battleDeck: state.player.battleDeck
+								deck: state.player.deck
 							}));
 						}
 					},
